333a50cdfce1d109db278c485a5febcf9cfd5aea
riyamathur0207 <riyamathur0207@gmail.com>
2024-07-23 22:24:51 -0700

    Moved to file1

--- file1.py
+++ file1.py
@@ -1,3 +1,17 @@
 #feature engineering, data preprocessing
 #TFIDF features, CountVectorizer features, etc
 #removing missing values from dataset, prepare dataset to be fed into model
+
+import pandas as pd
+import collections
+
+#Load datasets
+train_haiti = pd.read_csv('haiti_train.csv')
+train_sandy = pd.read_csv('sandy_train.csv')
+
+#countWordFreq
+texts = train_haiti['Text'].str.cat(sep=' ')
+words = texts.split()
+word_counts = collections.Counter(words)
+top_words = word_counts.most_common(30)
+print(top_words)
--- maincode.py
+++ maincode.py
@@ -1,14 +1,2 @@
-import pandas as pd
-import collections
 
-#Load datasets
-train_haiti = pd.read_csv('haiti_train.csv')
-train_sandy = pd.read_csv('sandy_train.csv')
 
-#countWordFreq
-texts = train_haiti['Text'].str.cat(sep=' ')
-words = texts.split()
-word_counts = collections.Counter(words)
-top_words = word_counts.most_common(30)
-print(top_words)
-# this is project pull example
