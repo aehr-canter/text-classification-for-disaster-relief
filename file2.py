5fa3ed90944db760c9bfa171aae055fb0884d2cf
riyamathur0207 <riyamathur0207@gmail.com>
2024-07-24 19:58:43 -0700

    Added more keywords

--- file2.py
+++ file2.py
@@ -3,41 +3,59 @@
 #evaluation metrics like accuracy, precision recall
 
 from file1 import tweets_haiti
+import re
+from nltk.stem import SnowballStemmer
+
 # initialize a list to store labels, with "NaN" as the default value
-labelsResults = ["NaN"] * len(tweets_haiti)
+#labelsResults = ["NaN"] * len(tweets_haiti)
 
+keywords = {
+    "Food": ["food", "hunger", "meal", "groceries", "nutrition", "eat", "hungry", "meals"],
+    "Water": ["water", "thirst", "thirsty", "hydration", "drink"],
+    "Energy": ["energy", "power", "electricity", "fuel", "gas", "battery", "heat"],
+    "Medical": ["medical", "health", "medicine", "doctor", "hospital", "clinic", "treatment", "virus"]
+}
 
+def preprocess_text(tweet):
+    tweet = tweet.lower()
+    tweet = re.sub(r'\W', ' ', tweet)
+    words = tweet.split()
+    return words
 
-def assign_label():
-    count = 0
-    # Iterate over each tweet in the Haiti dataset
-    for tweet in tweets_haiti:
-        # Convert tweet to lowercase
-        tweet.lower()
-        # Split the tweet into individual words
-        words = tweet.split()
+tweets_haiti_preprocessed = [preprocess_text(tweet) for tweet in tweets_haiti]
+
+def assign_label(words, keywords):
+    for label, kw_list in keywords.items():
+        if any(word in words for word in kw_list):
+            return label
+    return "NaN"
+
+labelsResults = [assign_label(words, keywords) for words in tweets_haiti_preprocessed]
+print(labelsResults)
+
+#     count = 0
+#     # Iterate over each tweet in the Haiti dataset
+#     for tweet in tweets_haiti:
+#         # Convert tweet to lowercase
+#         tweet.lower()
+#         # Split the tweet into individual words
+#         words = tweet.split()
         
-        # Assign labels based on keywords found in the tweet
-        if "food" in words:
-            labelsResults[count] = "Food"
-        elif "water" in words:
-            labelsResults[count] = "Water"
-        elif "energy" in words:
-            labelsResults[count] = "Energy"
-        elif "medical" in words:
-            labelsResults[count] = "Medical"
+#         # Assign labels based on keywords found in the tweet
+#         if "food" in words:
+#             labelsResults[count] = "Food"
+#         elif "water" in words:
+#             labelsResults[count] = "Water"
+#         elif "energy" in words:
+#             labelsResults[count] = "Energy"
+#         elif "medical" in words:
+#             labelsResults[count] = "Medical"
 
-        # Increment the count to move to the next label
-        count+=1
-        # Print the final list of labels for the Haiti tweets
-        print(labelsResults)
-        #checking if it worked for each tweet (total 1601)
-        print(len(labelsResults))
+#         # Increment the count to move to the next label
+#         count+=1
+#         # Print the final list of labels for the Haiti tweets
+#         print(labelsResults)
+#         #checking if it worked for each tweet (total 1601)
+#         print(len(labelsResults))
 
-assign_label()
-
-#countWordFreq
-# words = texts.split()
-# word_counts = collections.Counter(words)
-# top_words = word_counts.most_common(30)
-# print(top_words)
+# #assign_label()
